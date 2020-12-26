63c82b121ccff6dcbb3659ab24d442c6850a88e0
JaneMoody <janeemoody2@gmail.com>
2020-12-26 04:15:29 -0500

    Fixed app.js

--- african-marketplace/src/App.js
+++ african-marketplace/src/App.js
@@ -2,30 +2,17 @@ import React from "react";
 import { Switch, Route } from "react-router-dom";
 import Homepage from "./Pages/homePage";
 import Login from "./ViewsPages/Login";
-import Signup from "./ViewsPages/Signup";
-import MainLogin from "./ViewsPages/MainLogin";
-
 const App = () => (
   <div>
     <Switch>
       <Route exact path="/">
-        {/* Default page (use localSorage to skip in already signed in) */}
+        {/* Default page (use localSorage to skip if already signed in) */}
         <Homepage />
       </Route>
 
       <Route path="/login">
         {/* default for logging in */}
         <Login />
-      </Route>
-
-      <Route path="/signup">
-        {/* default for signing up */}
-        <Signup />
-      </Route>
-
-      <Route path="/recipes">
-        {/* default after sign in */}
-        <MainLogin />
       </Route>
     </Switch>
   </div>
