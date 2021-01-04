6d1168d3003e6429a496df06617d4dc433bb9c45
Shazeen Fabius <shazeenfabius@Shazeens-Mini.lan>
2021-01-04 14:36:17 -0800

    set up routing to sign in and home page

--- african-marketplace/src/App.js
+++ african-marketplace/src/App.js
@@ -1,10 +1,16 @@
+import { Route, Switch } from "react-router-dom";
 import "./App.css";
+import Homepage from "./components/homePage";
 import SignIn from "./components/Sign-in";
 
 function App() {
   return (
     <div className="App">
-      <SignIn />
+      <Switch>
+        <Route path="/sign-in" component={SignIn} />
+        {/* <Route path="/signup" component={} /> */}
+        <Route path="/" component={Homepage} />
+      </Switch>
     </div>
   );
 }
