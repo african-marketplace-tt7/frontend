ad90651442b2ac8c6a12ff51a746e12ce6089ad0
mxhs <maxwellstofman@gmail.com>
2021-01-06 14:02:18 -0800

    SignUp Form Works and redirects to app

--- african-marketplace/src/App.js
+++ african-marketplace/src/App.js
@@ -11,20 +11,17 @@ import SignIn from "./components/Sign-in";
 import SignupForm from "./components/SignupForm/SignupForm";
 
 function App() {
-	const [loggedIn, setIsLoggedIn] = useState(false);
-
 	return (
 		<div className="App">
 			<Switch>
 				{/* App Front End */}
 				<div id="page-wrapper">
 					<PrivateRoute exact path="/dashboard" component={Dashboard} />
+					<ThemeProvider theme={siteTheme}>
+						<Route exact path="/login" component={SignIn} />
+						<Route exact path="/signup" component={SignupForm} />
+					</ThemeProvider>
 				</div>
-
-				<ThemeProvider theme={siteTheme}>
-					<Route exact path="/login" component={SignIn} />
-					<Route exact path="/signup" component={SignupForm} />
-				</ThemeProvider>
 			</Switch>
 		</div>
 	);
