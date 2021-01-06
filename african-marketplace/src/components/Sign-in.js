a2b4a81700f0818ff37cd387d1ef32141e0c783b
Shane Slone <slone.shane@gmail.com>
2021-01-06 18:18:25 -0500

    fixed the login to work with OAuth2

--- african-marketplace/src/components/Sign-in.js
+++ african-marketplace/src/components/Sign-in.js
@@ -80,7 +80,17 @@ function SignIn(props) {
   const formSubmit = event => {
     event.preventDefault();
     axios
-      .post('https://african-marketplace-tt7.herokuapp.com/login', formState)
+      .post(
+        'https://african-marketplace-tt7.herokuapp.com/login',
+        `grant_type=password&username=${formState.username}&password=${formState.password}`,
+        {
+          headers: {
+            // btoa is converting our client id/client secret into base64
+            Authorization: `Basic ${btoa('lambda-client:lambda-secret')}`,
+            'Content-Type': 'application/x-www-form-urlencoded',
+          },
+        }
+      )
       .then(res => {
         localStorage.setItem('token', res.data.access_token);
         push('/dashboard');
