f5cc29b58487bf0df845533b65de02a614ed0f36
mxhs <maxwellstofman@gmail.com>
2021-01-06 12:19:35 -0800

    Deployment Fixes

--- african-marketplace/src/components/homePage.js
+++ african-marketplace/src/components/homePage.js
@@ -1,25 +1,22 @@
-import '../assets/css/main.css';
-import React from 'react'
-import {connect} from 'react-redux'
-import {NavLink} from 'react-router-dom'
+import "../assets/css/main.css";
+import React from "react";
+import { connect } from "react-redux";
+import { NavLink } from "react-router-dom";
 
 //Store
-import {fetchCurrentUser} from '../store/actions/currentUserActions'
+import { fetchCurrentUser } from "../store/actions/currentUserActions";
 
 // Components
-import HeaderNav from './HeaderNav'
-import {Button} from "./Button"
+import HeaderNav from "./HeaderNav";
+import { Button } from "./Button";
 
 const HomePage = (props) => {
+	return (
+		<div>
+			<HeaderNav />
+			<h1> this is app</h1>
 
-  return (
-    <div>
-      <HeaderNav/>
-      <h1> this is app</h1>
-      
-
-
-      {/* <Button title="Click me" type="primary" onClick={() => {
+			{/* <Button title="Click me" type="primary" onClick={() => {
       console.log("Click")
       }} size="large"/>
       <Button title="Click me" type="primary" onClick={() => {
@@ -28,19 +25,16 @@ const HomePage = (props) => {
       <Button title="Click me" type="primary" onClick={() => {
       console.log("Click")
       }} size="small"/> */}
-
-
-    </div>
-  );
-}
+		</div>
+	);
+};
 
 const mapStateToProps = (state) => {
-  return {
-    currentUser: state.combineReducers
-  }
-}
+	return {
+		currentUser: state.combineReducers,
+	};
+};
 
 export default connect(mapStateToProps, {
-  fetchCurrentUser
+	fetchCurrentUser,
 })(HomePage);
-
